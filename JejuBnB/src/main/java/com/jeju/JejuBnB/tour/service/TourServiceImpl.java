607f7e510f1c23e5e1c9d22734932c1011c24982
82103 <82103@DESKTOP-U5PI7FA>
2020-09-07 11:28:44 +0900

    reservationService

--- JejuBnB/src/main/java/com/jeju/JejuBnB/tour/service/TourServiceImpl.java
+++ JejuBnB/src/main/java/com/jeju/JejuBnB/tour/service/TourServiceImpl.java
@@ -1,14 +1,5 @@
 package com.jeju.JejuBnB.tour.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.jeju.JejuBnB.tour.model.dao.TourDao;
-
-@Service("TourService")
-public class TourServiceImpl implements TourService{
-	
-	@Autowired
-	private TourDao tourDao;
+public class TourServiceImpl {
 
 }

d6f245ddf75deb821a151785a1cf1d960b118e9c
82103 <82103@DESKTOP-U5PI7FA>
2020-09-09 18:15:57 +0900

    커밋

--- JejuBnB/src/main/java/com/jeju/JejuBnB/reservation/model/service/ReservationService.java
+++ JejuBnB/src/main/java/com/jeju/JejuBnB/reservation/model/service/ReservationService.java
@@ -7,5 +7,5 @@ public interface ReservationService {
 	Room selectOne(int room_no); //선택한 숙소 리스트
 	int insertReserv(Reservation reserv); //날짜&인원 정보 넘기기
 	Reservation selectReserv(int room_no);  //넘긴 정보와 숙소 정보 리스트
-	Room selectReservWait(int room_no); //결제대기 리스트 
+	Room selectReservWait(int room_no); //결제대기 리스트
 }
